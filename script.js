f22523b5ed2db2d464caa82efed360850e7c2ff0
Prince <princejaiswal6106@gmail.com>
2026-09-23 00:33:38 +0530

    leran javascript

---
+++ script.js
@@ -0,0 +1,37 @@
+// let a = 10;
+
+// console.log(a);
+// console.warn("Somthing went wrong");
+// console.error("error 404");
+
+// const a = 10;
+// a = 20;
+// console.log(a);
+
+// var a = 10;
+// if(true){
+//     let b = 20;
+//     const c =30;
+
+//     console.log(a);
+// }
+
+// console.log(a, b, c);
+
+// Data Types
+// Primitive data types
+let name = "Prince"
+let n = 19;
+console.log(typeof(a))
+console.log(typeof(n))
+
+// non-primitive data type
+let student ={
+    Name: "Prince",
+    Age: 19,
+}
+
+console.log(student)
+console.log(student.Name)
+console.log(student.Age)
+console.log(typeof(student))
